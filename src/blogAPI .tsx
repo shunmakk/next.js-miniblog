346fe3b4122bcd0cb3377c7abe994fba8957299a
shunma <shunmakajioka12@gmail.com>
2024-02-19 21:45:35 +0900

    エラーページの作成

--- src/blogAPI .tsx
+++ src/blogAPI .tsx
@@ -3,6 +3,9 @@ import { Article } from "./types"
 
 export   const getALLArticles = async():Promise<Article[]> => {
      const res = await fetch(`http://localhost:3001/posts`,{cache: "no-store"}); //SSR　更新頻度が高いから
+
+     if(!res.ok)
+     throw new   Error("エラーが発生しました");
      
      const articles = await res.json();
      return articles;
