3789ee35c504134f768bbfc5dd05489249eb9415
shunma <shunmakajioka12@gmail.com>
2024-02-21 22:46:41 +0900

    名称を変更

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='flex items-center justify-center mt-32'>ブログが投稿されていません</div>
+    <div className='flex items-center justify-center mt-32'>このブログは無効です</div>
   )
 }
 
