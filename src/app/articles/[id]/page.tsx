039386525c66dacc1af50a91545a7cb39faed659
shunma <shunmakajioka12@gmail.com>
2024-02-22 16:14:30 +0900

    削除用apiの追加

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -13,7 +13,6 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const res = await fetch(`${API_URL}/api/${params.id}` ,{next: {revalidate: 60}}); 
 
 
-
 const detailArticle = await res.json();
 
 
@@ -22,7 +21,7 @@ const detailArticle = await res.json();
   
   return (
     <div className='max-w-3xl mx-auto p-5'>
-      <Image src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`} alt="トップ画像" width={1280} height={300}/>
+      <Image src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`} alt="トップ画像" width={1280} height={300} priority  />
       <h1 className='text-4xl text-center mb-10 mt-10'>{detailArticle.title}</h1>
       <div className='text-lg leading-relaxed text-justify'>
         <p>{detailArticle.content}</p>
--- src/pages/api/[id].tsx
+++ src/pages/api/[id].tsx
@@ -4,6 +4,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { id } = req.query;
 
+    
+    switch(req.method){
+      case 'GET':
+
     const { data, error } = await supabase
         .from("posts")
         .select("*")
@@ -19,6 +23,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     return res.status(200).json(data);
+
+
+   //削除用のAPI
+    case "DELETE":
+    const  {error: deleteError} = await supabase
+    .from('posts')
+    .delete()
+    .eq("id", id);
+
+
+    if(deleteError){
+        return res.status(500).json({error : deleteError.message});
+    }
+    return res.status(200).json({message: '削除に成功しました'})
+
+}
 }
 
 export const config = {
@@ -26,3 +46,45 @@ export const config = {
         bodyParser: false,
     },
 };
+
+
+// import { supabase } from "@/utils/supabaseClient";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { notFound } from "next/navigation";
+
+// export default async function handler(
+// req: NextApiRequest,
+//   res: NextApiResponse
+//  ) {
+//   const { id } = req.query;
+//   console.log(id);
+//    switch (req.method) {   case "GET":
+//      const { data, error } = await supabase
+//         .from("posts")
+//         .select("*")
+//        .eq("id", id)
+//         .single();
+
+//     if (error) return res.status(500).json({ error: error.message });
+//     if (!data) {
+//        notFound();
+//     }
+
+//       return res.status(200).json(data);
+
+ 
+
+//  case "DELETE":
+//       const { error: deleteError } = await supabase
+//      .from("posts")
+//       .delete()
+//        .eq("id", id);
+
+//       if (deleteError)
+//       return res.status(500).json({ error: deleteError.message });
+//     return res.status(200).json({ message: "Deleted successfully" });
+
+//     default:
+//        res.status(405).end();
+//    }
+// }
